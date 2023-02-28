9e1f43a0379b846db4a31354e68331b0c7408392
kiyo <i.mitsuki19@gmail.com>
2023-02-28 09:30:25 +0900

    DAOを定義

---
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -0,0 +1,23 @@
+package com.example.busschedule.database.schedule
+
+import androidx.room.Dao
+import androidx.room.Query
+
+/**
+ * DAO で関数を呼び出すことは、データベースで SQL コマンドを実行することと同じです
+ */
+@Dao
+interface ScheduleDao {
+    /**
+     * 昇順 > ASC / ascending order
+     * 降順 > DESC / descending order
+     */
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getAll(): List<Schedule>
+
+    /**
+     * クエリの前にコロン（:）を付けると（例: 関数パラメータの :stopName）、Kotlin 値を参照できます
+     */
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
+    fun getByStopName(stopName: String): List<Schedule>
+}
